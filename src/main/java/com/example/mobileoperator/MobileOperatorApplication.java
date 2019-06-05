e75421868e78c61f90a72a92294bc42b1bf76e9f
CaH4aZzz <CaH4aZzz@ua.fm>
2019-06-06 02:41:58 +0300

    initial commit

---
+++ src/main/java/com/example/mobileoperator/MobileOperatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.mobileoperator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileOperatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MobileOperatorApplication.class, args);
+    }
+
+}
